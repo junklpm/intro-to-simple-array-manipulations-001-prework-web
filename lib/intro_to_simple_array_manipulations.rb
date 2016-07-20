f6758f913ba99da993c237a4c0b9773a152a203f
junklpm <lance.p.miller@gmail.com>
2016-07-19 21:31:27 -0400

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,56 @@
+def using_push(countries_in_west_africa, next_country)
+  updated_array = []
+  updated_array.push(next_country)
+  updated_array
+end 
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  @updated_array = []
+  @updated_array.unshift(new_neighborhood)
+  @updated_array += neighborhoods_in_northwest_brooklyn
+
+end
+
+def using_pop(great_hits_of_the_nineties)
+  great_hits_of_the_nineties.pop
+end
+
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+  list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+
+end
+
+def using_uniq(captain_planet_and_the_planeteers)
+  captain_planet_and_the_planeteers.uniq
+  
+end
+
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork.flatten
+
+  end
+
+  def using_delete(instructors, no_offense_steven)
+    instructors.delete("Steven")
+
+  end
+
+def using_delete_at(famous_robots, deleted_robot)
+  famous_robots.delete_at(2)
+end
